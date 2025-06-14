23557fd3ec6a5d5ba01546d5954043b440a56104
HeyShivansh <shivabalyan123@gmail.com>
2025-06-14 22:05:04 +0530

    13th June 2025

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const startDate = new Date('2025-05-24');
             '2025-06-10': "Neural Networks ✅ \n Activation function✅ \n Gradient descent for Neural Networks✅",
             '2025-06-11': "Back Propagation✅ \n L Layer Neural Networks ✅ \n HyperParameters✅",
             '2025-06-12': "Practied on a jupyter notebook of L layer neural network",
-            '2025-06-13': "We're not there yet",
+            '2025-06-13': "Regularization✅ \n Normalization ✅ \n Weighted intialization✅ \n Gradient Checking✅",
             '2025-06-14': "We're not there yet",
             '2025-06-15': "We're not there yet",
             '2025-06-16': "We're not there yet",

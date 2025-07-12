8ca4c25e62c84f6a3c9a11b66a6b21925e133b27
HeyShivansh <shivabalyan123@gmail.com>
2025-07-12 13:32:27 +0530

    9th July 2025

--- script.js
+++ script.js
@@ -57,9 +57,8 @@ const startDate = new Date('2025-05-24');
             '2025-07-06': "Completed 8 hours of 12 hr JS one shot",
             '2025-07-07': "Finished JS one shot",
             '2025-07-08': "Developed the convolutional layer for the project",
-            '2025-07-09': "We're not there yet",
-            '2025-07-10': "We're not there yet",
-            '2025-07-11': "We're not there yet",
+            '2025-07-09': "Revised Arrays concepts and practiced Questions",
+           
             '2025-07-12': "We're not there yet"
         };
         localStorage.setItem('progressData', JSON.stringify({}));
